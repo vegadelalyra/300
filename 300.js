6c450a9d41b71378e2b1b13f35901a2ec2a51d09
Daniel Vega <98dvega@gmail.com>
2021-01-10 00:48:34 -0500

    se avanzó a siguiente sección, se corrigieron bugs

--- 300.js
+++ 300.js
@@ -1,3 +1,5 @@
+// 6 EJERCICIOS DE VARIABLES
+
 "1." 
     a = 10
     b = 20
@@ -100,3 +102,217 @@ document.getElementById("1").innerHTML = `
   d = a + b - c + d
   document.getElementById("6").innerHTML = `
   a = ${a}\n\n  b = ${b}\n\n  c = ${c}\n\n  d = ${d}`; 
+
+
+// 20 EJERCICIOS DE OPERADORES
+  a = 0 
+  b = 0
+  c = 0
+  d = 0
+
+"1." 
+    
+    a = 10
+    b = 20
+    c = 10
+    a = a + 15
+    b = b + 12
+    c = a * c
+  
+    "¿Qué valores quedan en las variables a, b y c?"
+  document.getElementById("301").innerHTML = `
+  a = ${a}\n\n  b = ${b}\n\n  c = ${c}\n\n`;     
+
+
+"2."
+    
+    a = 3
+    b = 8
+    c = 1
+    a = 5
+    b = 9
+    c = 7
+    a = a + 1
+    b = b + 2
+    c = c + 3
+
+    "¿Qué valores quedan en las variables a, b y c?"
+  document.getElementById("302").innerHTML = `
+  a = ${a}\n\n  b = ${b}\n\n  c = ${c}\n\n`; 
+
+"3." 
+    
+    a = 10
+    b = 5
+    c = 10
+    a = a + b - 5
+    b = a + b - 5
+    c = a + b - 5
+    a = a + 5 * b / 2
+    b = a + 5 * b / 2
+    c = a + 5 * b / 2
+
+    "¿Qué valores quedan en las variables a, b y c?"
+  document.getElementById("303").innerHTML = `
+  a = ${a}\n\n  b = ${b}\n\n  c = ${c}\n\n`; 
+
+"4." 
+    
+    a = 5
+    b = 5
+    c = 5
+    a = a + a
+    b = b + b
+    c = c + c
+    a = a + b + c
+    b = a + b + c
+    c = a + b + c
+
+    "¿Qué valores quedan en las variables a, b y c?"
+  document.getElementById("304").innerHTML = `
+  a = ${a}\n\n  b = ${b}\n\n  c = ${c}\n\n`; 
+
+"5."
+
+    a = 10
+    b = 10
+    c = 10
+    a = a + 5
+    b = a + 3
+    c = a + 2
+    a = b + 4
+    b = b + 5
+    c = c + 8
+    
+    "¿Qué valores quedan en las variables a, b y c?"
+  document.getElementById("305").innerHTML = `
+  a = ${a}\n\n  b = ${b}\n\n  c = ${c}\n\n`; 
+
+"6." 
+
+    a = 10
+    b = 1
+    c = 4
+    a = a + c
+    b = a + c
+    c = a + c
+    a = c + 5
+    b = c + b
+    c = a + b + c
+    
+    "¿Qué valores quedan en las variables a, b y c?"
+  document.getElementById("306").innerHTML = `
+  a = ${a}\n\n  b = ${b}\n\n  c = ${c}\n\n`; 
+
+"7." 
+    
+    a = 1
+    b = 1
+    c = 1
+    a = a + a
+    b = b + a
+    c = c + a
+    a = a + a
+    b = b + a
+    c = c + a
+    
+    "¿Qué valores quedan en las variables a, b y c?"
+  document.getElementById("307").innerHTML = `
+  a = ${a}\n\n  b = ${b}\n\n  c = ${c}\n\n`; 
+
+"8." 
+
+    a = 10
+    b = 50
+    c = 30
+    a = a - b
+    b = b - c
+    c = c - a
+    a = a - 1
+    b = b - a
+    c = c + a - b
+    
+    "¿Qué valores quedan en las variables a, b y c?"
+  document.getElementById("308").innerHTML = `
+  a = ${a}\n\n  b = ${b}\n\n  c = ${c}\n\n`; 
+
+"9." 
+    
+    a = 1
+    b = 2
+    c = 3
+    a = a + b
+    b = a - b
+    c = a * b
+    a = a - b
+    b = a + b
+    c = a * b
+
+    "¿Qué valores quedan en las variables a, b y c?"
+  document.getElementById("309").innerHTML = `
+  a = ${a}\n\n  b = ${b}\n\n  c = ${c}\n\n`; 
+
+"10." 
+    
+    a = 1
+    b = 2
+    c = 3
+    a = a + 2
+    b = a + 2 + b
+    c = a + 2 + c
+    a = a / 2
+    b = b / 2
+    c = c / 2
+    "¿Qué valores quedan en las variables a, b y c?"  
+  document.getElementById("310").innerHTML = `
+  a = ${a}\n\n  b = ${b}\n\n  c = ${c}\n\n`; 
+
+
+"11."
+
+ x = (a+b/c)/(a/b+c)
+
+
+"12."
+
+x = (a + b + a/b)/c
+
+
+"13."
+
+x = a/(a+b)/a/(a-b)
+
+
+"14."
+
+x = (a+b/(a+b+b/c))/a+b/(c+a)
+
+
+"15."
+
+x = (a+b+c)/(a+(b/c))
+
+
+"16."
+
+x = (a+b+c/(d*a))/(a+b*(c/d))
+
+
+"17."
+
+x = (a+b/c+d)/a
+
+
+"18."
+
+x = (a/b+b/c)/(a/b-b/c)
+
+
+"19."
+
+x = a + (a+(a+b/c+d))/(a+a/b)
+
+
+"20."
+
+x = a + b + c/d + (a/(b-c)/a/(b+c))
